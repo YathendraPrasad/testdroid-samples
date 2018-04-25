d5aebeb9f2823d516f8f2e913a6b148bdd2b80e2
Yathendra Prasad <yathendraprasad@yathendra.local>
2018-04-25 12:48:42 +0530

    Updated test class and property file

--- appium/sample-scripts/java/src/test/java/com/testdroid/appium/android/sample/AndroidAppiumExampleTest.java
+++ appium/sample-scripts/java/src/test/java/com/testdroid/appium/android/sample/AndroidAppiumExampleTest.java
@@ -27,12 +27,19 @@ public class AndroidAppiumExampleTest  extends BaseAndroidTest {
     public void mainPageTest() throws IOException, InterruptedException {
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         takeScreenshot("start");
-        wd.findElement(By.xpath("//android.widget.RadioButton[@text='Use Testdroid Cloud']")).click();
+        /*wd.findElement(By.xpath("//android.widget.RadioButton[@text='Use Testdroid Cloud']")).click();
         wd.findElement(By.xpath("//android.widget.EditText[@resource-id='com.bitbar.testdroid:id/editText1']")).sendKeys("John Doe");
         takeScreenshot("after_adding_name");
         wd.navigate().back();
         wd.findElement(By.xpath("//android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.Button[1]")).click();
-        takeScreenshot("after_answer");
+        takeScreenshot("after_answer");*/
+        
+        wd.findElement(By.xpath("//android.widget.EditText[@resource-id='com.sample.resourcerequestandroid:id/resource']")).sendKeys("Endpoint");
+        takeScreenshot("after_entering_resource_endpoint");
+        wd.findElement(By.xpath("//android.widget.LinearLayout[1]/android.widget.Button[1]")).click();
+        takeScreenshot("after_clicking_button");
+        
+        
     }
 
 }
